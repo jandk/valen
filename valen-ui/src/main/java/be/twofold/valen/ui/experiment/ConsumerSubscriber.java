70bf2f616fc6a7ed72cbe72c819386e631070c67
Jan De Kock <jan@twofold.be>
2023-12-29 20:32:50 +0100

    fix: More ui tests

--- valen-ui/src/main/java/be/twofold/valen/ui/experiment/ConsumerSubscriber.java
+++ valen-ui/src/main/java/be/twofold/valen/ui/experiment/ConsumerSubscriber.java
@@ -1,38 +1,44 @@
 package be.twofold.valen.ui.experiment;
 
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 import java.util.function.*;
 
-final class ConsumerSubscriber<T> implements Flow.Subscriber<T> {
-    final Consumer<? super T> consumer;
-    Flow.Subscription subscription;
+public final class ConsumerSubscriber<T> implements Flow.Subscriber<T> {
+    private final AtomicBoolean subscribed = new AtomicBoolean();
+    private final Consumer<T> consumer;
+    private Flow.Subscription subscription;
 
-    ConsumerSubscriber(Consumer<? super T> consumer) {
-        this.consumer = consumer;
+    public ConsumerSubscriber(Consumer<T> consumer) {
+        this.consumer = Objects.requireNonNull(consumer);
     }
 
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
+        Objects.requireNonNull(subscription);
+        if (!subscribed.compareAndSet(false, true)) {
+            subscription.cancel();
+            return;
+        }
         this.subscription = subscription;
-        subscription.request(Long.MAX_VALUE);
-    }
-
-    @Override
-    public void onError(Throwable ex) {
-        ex.printStackTrace();
-    }
-
-    @Override
-    public void onComplete() {
-        System.out.println("Completed");
+        subscription.request(1);
     }
 
     @Override
     public void onNext(T item) {
-        try {
-            consumer.accept(item);
-        } catch (Throwable ex) {
-            subscription.cancel();
-        }
+        Objects.requireNonNull(item);
+        consumer.accept(item);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.err.println("Error: " + throwable.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+        System.err.println("Done");
     }
 }
--- valen-ui/src/main/java/be/twofold/valen/ui/experiment/PublishingButton.java
+++ valen-ui/src/main/java/be/twofold/valen/ui/experiment/PublishingButton.java
@@ -1,21 +1,38 @@
 package be.twofold.valen.ui.experiment;
 
 import javax.swing.*;
+import java.awt.event.*;
 import java.util.concurrent.*;
 import java.util.function.*;
 
 public class PublishingButton extends JButton {
-    private final SubmissionPublisher<ButtonAction> publisher
-        = new SubmissionPublisher<>(Executors.newVirtualThreadPerTaskExecutor(), Flow.defaultBufferSize());
+    private final SubmissionPublisher<ButtonAction> publisher = new ButtonPublisher();
 
     public PublishingButton() {
-        addActionListener(e -> {
-            System.out.println(Thread.currentThread() + ": Submitting");
-            publisher.submit(new ButtonAction());
-        });
     }
 
     public void subscribe(Consumer<ButtonAction> actionConsumer) {
         publisher.subscribe(new ConsumerSubscriber<>(actionConsumer));
+    }
+
+    private final class ButtonPublisher extends SubmissionPublisher<ButtonAction> {
+        private final ActionListener listener;
+
+        ButtonPublisher() {
+            super(Executors.newThreadPerTaskExecutor(Executors.defaultThreadFactory()), Flow.defaultBufferSize());
+
+            listener = e -> {
+                System.out.println(Thread.currentThread() + ": Submitting");
+                submit(new ButtonAction());
+            };
+
+            addActionListener(listener);
+        }
+
+        @Override
+        public void close() {
+            super.close();
+            removeActionListener(listener);
+        }
     }
 }
