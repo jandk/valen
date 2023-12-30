f52b9b727fc0f6d36fd940f4178ecd617b119421
Jan De Kock <jan@twofold.be>
2023-12-30 01:03:40 +0100

    refactor: PngWriter now writes Textures, just like DdsWriter

--- valen-core/src/main/java/be/twofold/valen/core/texture/Surface.java
+++ valen-core/src/main/java/be/twofold/valen/core/texture/Surface.java
@@ -1,8 +1,28 @@
 package be.twofold.valen.core.texture;
 
+import be.twofold.valen.core.util.*;
+
 public record Surface(
     int width,
     int height,
     byte[] data
 ) {
+    public Surface {
+        Check.argument(width > 0, "width must be greater than 0");
+        Check.argument(height > 0, "height must be greater than 0");
+        Check.notNull(data, "data is null");
+
+        // TODO: check data length
+        // Can't do that right now, because we might need the actual format,
+        // but even then calculating the actual length is a PITA for a lot of formats
+    }
+
+    @Override
+    public String toString() {
+        return "Surface(" +
+            "width=" + width + ", " +
+            "height=" + height + ", " +
+            "data=[" + data.length + " bytes]" +
+            ")";
+    }
 }
--- valen-lib/src/main/java/be/twofold/valen/writer/png/PngWriter.java
+++ valen-lib/src/main/java/be/twofold/valen/writer/png/PngWriter.java
@@ -1,19 +1,55 @@
 package be.twofold.valen.writer.png;
 
+import be.twofold.valen.converter.decoder.*;
+import be.twofold.valen.core.texture.*;
+
 import java.io.*;
 import java.nio.channels.*;
 
 public final class PngWriter {
     private final WritableByteChannel channel;
+    private final boolean normalizeNormalMap;
 
     public PngWriter(WritableByteChannel channel) {
-        this.channel = channel;
+        this(channel, false);
     }
 
-    public void write(Png png) throws IOException {
-        try (PngOutputStream output = new PngOutputStream(
-            Channels.newOutputStream(channel), png.format())) {
-            output.writeImage(png.data());
+    public PngWriter(WritableByteChannel channel, boolean normalizeNormalMap) {
+        this.channel = channel;
+        this.normalizeNormalMap = normalizeNormalMap;
+    }
+
+    public void write(Texture texture) throws IOException {
+        var format = mapPngFormat(texture);
+        var decoder = mapDecoder(texture.format());
+        var data = decoder.decode(texture.surfaces().getFirst());
+
+        try (var output = new PngOutputStream(Channels.newOutputStream(channel), format)) {
+            output.writeImage(data);
         }
+    }
+
+    private BCDecoder mapDecoder(TextureFormat format) {
+        return switch (format) {
+            case Bc1Unorm, Bc1UnormSrgb, Bc1Typeless -> new BC1Decoder(3, 0, 1, 2);
+            case Bc3Unorm, Bc3UnormSrgb, Bc3Typeless -> new BC3Decoder(4, 0, 1, 2, 3);
+            case Bc4Unorm, Bc4Typeless -> new BC4UDecoder(1, 0);
+            case Bc5Unorm, Bc5Typeless -> new BC5UDecoder(3, 0, 1, 2, normalizeNormalMap);
+            default -> throw new UnsupportedOperationException("Unsupported format: " + format);
+        };
+    }
+
+    private PngFormat mapPngFormat(Texture texture) {
+        var width = texture.width();
+        var height = texture.height();
+        return switch (texture.format()) {
+            case Bc1Unorm, Bc1Typeless, Bc5Unorm, Bc5Typeless ->
+                new PngFormat(width, height, PngColorType.Rgb, 8, true);
+            case Bc1UnormSrgb -> new PngFormat(width, height, PngColorType.Rgb, 8, false);
+            case Bc3Unorm, Bc3Typeless -> new PngFormat(width, height, PngColorType.RgbAlpha, 8, true);
+            case Bc3UnormSrgb -> new PngFormat(width, height, PngColorType.RgbAlpha, 8, false);
+            case Bc4Unorm, Bc4Typeless -> new PngFormat(width, height, PngColorType.Gray, 8, true);
+            default -> throw new UnsupportedOperationException("Unsupported format: " + texture.format());
+        };
     }
 }
