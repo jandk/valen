1986eb2ab09a81d44574b02dae55c2d64ac25a1d
Jan De Kock <jan@twofold.be>
2024-01-03 00:13:33 +0100

    fix: Need the parser for now

--- valen-lib/src/main/java/module-info.java
+++ valen-lib/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module valen.lib {
     exports be.twofold.valen.hash;
     exports be.twofold.valen.manager;
     exports be.twofold.valen.oodle;
+    exports be.twofold.valen.reader.decl.parser;
     exports be.twofold.valen.reader.image;
     exports be.twofold.valen.reader.md6;
     exports be.twofold.valen.reader.md6anim;
