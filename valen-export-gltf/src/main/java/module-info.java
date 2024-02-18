b594f3f9c35d2970d72a0fb976a88e991a009c30
REDxEYE <med45c@gmail.com>
2024-02-19 02:05:20 +0300

    Made "requires" for org.immutables.value "static"

--- valen-export-gltf/src/main/java/module-info.java
+++ valen-export-gltf/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module valen.export.gltf {
     requires com.google.gson;
-    requires org.immutables.value;
+    requires static org.immutables.value;
     requires valen.core;
 
     exports be.twofold.valen.export.gltf.model to com.google.gson, org.immutables.value;
