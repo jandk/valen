45f355824de54721386afd61e6ad6b3b43114f58
REDxEYE <med45c@gmail.com>
2024-02-19 15:19:36 +0300

    Matrix/Quaternion/Vector fields may be null, these adapters must be nullsafe

--- valen-export-gltf/src/main/java/be/twofold/valen/export/gltf/GltfWriter.java
+++ valen-export-gltf/src/main/java/be/twofold/valen/export/gltf/GltfWriter.java
@@ -23,11 +23,11 @@ public final class GltfWriter implements GltfContext {
         .registerTypeAdapter(AnimationChannelTargetPath.class, new AnimationChannelTargetPathTypeAdapter())
         .registerTypeAdapter(AnimationSamplerInterpolation.class, new AnimationSamplerInterpolationTypeAdapter())
         .registerTypeAdapter(BufferViewTarget.class, new BufferViewTargetTypeAdapter().nullSafe())
-        .registerTypeAdapter(Matrix4.class, new Matrix4TypeAdapter())
-        .registerTypeAdapter(Quaternion.class, new QuaternionTypeAdapter())
-        .registerTypeAdapter(Vector2.class, new Vector2TypeAdapter())
-        .registerTypeAdapter(Vector3.class, new Vector3TypeAdapter())
-        .registerTypeAdapter(Vector4.class, new Vector4TypeAdapter())
+        .registerTypeAdapter(Matrix4.class, new Matrix4TypeAdapter().nullSafe())
+        .registerTypeAdapter(Quaternion.class, new QuaternionTypeAdapter().nullSafe())
+        .registerTypeAdapter(Vector2.class, new Vector2TypeAdapter().nullSafe())
+        .registerTypeAdapter(Vector3.class, new Vector3TypeAdapter().nullSafe())
+        .registerTypeAdapter(Vector4.class, new Vector4TypeAdapter().nullSafe())
         .create();
 
     private final WritableByteChannel channel;
@@ -172,5 +172,4 @@ public final class GltfWriter implements GltfContext {
             throw new UncheckedIOException(e);
         }
     }
-
 }
