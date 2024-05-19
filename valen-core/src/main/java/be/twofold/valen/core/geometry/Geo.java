c3fcb0ea1a09b40f8cd4c1a5ff29fb5d613fdcc3
Jan De Kock <jan@twofold.be>
2024-05-20 01:18:10 +0200

    fix: Gotta flip the faces

--- valen-core/src/main/java/be/twofold/valen/core/geometry/Geo.java
+++ valen-core/src/main/java/be/twofold/valen/core/geometry/Geo.java
@@ -8,8 +8,13 @@ import java.nio.*;
 import java.util.*;
 
 public final class Geo {
+    private final boolean invertFaces;
 
-    public static Mesh readMesh(DataSource source, List<Accessor> vertexAccessors, Accessor faceAccessor) throws IOException {
+    public Geo(boolean invertFaces) {
+        this.invertFaces = invertFaces;
+    }
+
+    public Mesh readMesh(DataSource source, List<Accessor> vertexAccessors, Accessor faceAccessor) throws IOException {
         var startPos = source.tell();
 
         var vertexBuffers = new HashMap<Semantic, VertexBuffer>();
@@ -22,13 +27,16 @@ public final class Geo {
 
         source.seek(startPos);
         var buffer = read(source, faceAccessor);
+        if (invertFaces) {
+            invertFaces((ShortBuffer) buffer);
+        }
         var faceBuffer = new VertexBuffer(buffer, faceAccessor.info());
 
         source.seek(startPos);
         return new Mesh(faceBuffer, vertexBuffers, -1);
     }
 
-    public static Buffer read(
+    private Buffer read(
         DataSource source,
         Accessor accessor
     ) throws IOException {
@@ -43,6 +51,18 @@ public final class Geo {
         return buffer.flip();
     }
 
+    private void invertFaces(ShortBuffer buffer) {
+        for (int i = 0, lim = buffer.capacity(); i < lim; i += 3) {
+            var temp = buffer.get(i);
+            buffer.put(i, buffer.get(i + 2));
+            buffer.put(i + 2, temp);
+        }
+    }
+
+    public interface Reader {
+        void read(DataSource source, Buffer buffer) throws IOException;
+    }
+
     public record Accessor(
         int offset,
         int count,
@@ -51,18 +71,4 @@ public final class Geo {
         Reader reader
     ) {
     }
-
-    public record AccessorInfo(
-        Semantic semantic,
-        ElementType elementType,
-        ComponentType componentType,
-        boolean normalized,
-        Reader reader
-    ) {
-    }
-
-    public interface Reader {
-        void read(DataSource source, Buffer buffer) throws IOException;
-    }
-
 }
