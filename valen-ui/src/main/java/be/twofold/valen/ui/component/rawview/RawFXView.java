7f7aedbfe6fa0ec3580eaf9114239790b24e91a0
Jan De Kock <jan@twofold.be>
2025-02-18 22:25:45 +0100

    Extract HexDump code, and use TextFlow, so we can have different colors

--- valen-ui/src/main/java/be/twofold/valen/ui/component/rawview/RawFXView.java
+++ valen-ui/src/main/java/be/twofold/valen/ui/component/rawview/RawFXView.java
@@ -7,32 +7,24 @@ import javafx.collections.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.scene.text.*;
 
-import java.nio.charset.*;
 import java.util.*;
+import java.util.stream.*;
 
 public final class RawFXView implements RawView, FXView {
-    private static final List<String> FONTS = List.of(
-        "Jetbrains Mono",
-        "Cascadia Mono",
-        "Consolas",
-        "Monospaced"
-    );
-    private static final Font MONOSPACED;
-
-    static {
-        MONOSPACED = FONTS.stream()
+    private static final Font MONOSPACED =
+        Stream.of("Jetbrains Mono", "Cascadia Mono", "Consolas", "Monospaced")
             .map(Font::font)
             .filter(font -> !Font.getDefault().equals(font))
             .findFirst().orElseThrow();
-    }
 
     private final VBox view = new VBox();
     private final ListView<Integer> binaryView = new ListView<>();
     private final TextArea textView = new TextArea();
 
-    private byte[] binary;
+    private HexDump hexDump;
 
     @Inject
     public RawFXView() {
@@ -45,12 +37,15 @@ public final class RawFXView implements RawView, FXView {
         // binaryView.addEventFilter(MouseEvent.MOUSE_PRESSED, Event::consume);
         binaryView.setCellFactory(_ -> new HexdumpListCell());
         binaryView.setFixedCellSize(20);
+        binaryView.setFocusModel(null);
+        binaryView.setSelectionModel(null);
 
         // Disable editing
         textView.setEditable(false);
         textView.setFont(MONOSPACED);
 
         // Little hack, but 8 is too much
+        // TODO: Find a better solution
         var styleText = "Text { -fx-tab-size: 4; }";
         var styleBase64 = Base64.getUrlEncoder().encodeToString(styleText.getBytes());
         var url = "data:text/css;charset=UTF-8;base64," + styleBase64;
@@ -67,7 +62,8 @@ public final class RawFXView implements RawView, FXView {
         textView.clear();
         view.getChildren().setAll(binaryView);
 
-        this.binary = binary;
+        // TODO: Choose themed color
+        hexDump = new HexDump(binary, MONOSPACED, Color.WHITE, Color.GRAY);
         int numRows = (binary.length + 15) / 16;
         binaryView.scrollTo(0);
         binaryView.setItems(new IndexObservableList(numRows));
@@ -75,89 +71,23 @@ public final class RawFXView implements RawView, FXView {
 
     @Override
     public void setText(String text) {
-        binary = null;
+        hexDump = null;
         binaryView.setItems(FXCollections.emptyObservableList());
         view.getChildren().setAll(textView);
         textView.setText(text);
     }
 
     private final class HexdumpListCell extends ListCell<Integer> {
-        private static final char[] HEX = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
-        };
-        private static final char[] CHARS;
-
-        static {
-            var bytes = new byte[256];
-            for (int i = 0; i < bytes.length; i++) {
-                bytes[i] = (byte) i;
-            }
-
-            CHARS = new char[256];
-            var charset = new String(bytes, Charset.forName("windows-1252")).toCharArray();
-            for (int i = 0; i < charset.length; i++) {
-                var ch = charset[i];
-                CHARS[i] = Character.getType(ch) != Character.CONTROL && ch != 0xFFFD ? ch : '.';
-            }
-        }
-
         @Override
         protected void updateItem(Integer item, boolean empty) {
             super.updateItem(item, empty);
             if (empty || item == null) {
-                setText(null);
+                setGraphic(null);
             } else {
-                setText(hexdump(item));
+                setGraphic(hexDump.hexdump(item));
             }
             setFont(MONOSPACED);
-        }
-
-        private String hexdump(int index) {
-            var chars = new char[78];
-            Arrays.fill(chars, ' ');
-
-            int offset = index * 16;
-            toHex(chars, 0, offset, 8);
-
-            dumpBytes(offset, chars, 10);
-            dumpBytes(offset + 8, chars, 35);
-            dumpAscii(offset, chars, 60);
-
-            return new String(chars);
-        }
-
-        private void dumpBytes(int offset, char[] chars, int charOffset) {
-            for (int i = 0; i < 8; i++) {
-                if (offset + i >= binary.length) {
-                    break;
-                }
-                int value = Byte.toUnsignedInt(binary[offset + i]);
-                toHex(chars, charOffset + i * 3, value, 2);
-            }
-        }
-
-        private void dumpAscii(int offset, char[] chars, int charOffset) {
-            chars[charOffset++] = '|';
-            for (int i = 0; i < 16; i++) {
-                if (offset + i >= binary.length) {
-                    break;
-                }
-                int value = Byte.toUnsignedInt(binary[offset + i]);
-                // char print = isPrintable(value) ? (char) value : '.';
-                char print = CHARS[value];
-                chars[charOffset++] = print;
-            }
-            chars[charOffset] = '|';
-        }
-
-        private static boolean isPrintable(int value) {
-            return (value > 0x1F && value < 0x7F) || value > 0x9F;
-        }
-
-        private void toHex(char[] chars, int offset, int value, int length) {
-            for (int i = length - 1, o = offset; i >= 0; i--, o++) {
-                chars[o] = HEX[(value >> (i * 4)) & 0xF];
-            }
+            setText(null);
         }
     }
 
