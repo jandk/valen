59834bd56a26bf702dd043b5a10484fd1e7ffb9e
Jan De Kock <jan@twofold.be>
2025-04-13 00:35:45 +0200

    Stick with merged filtering, as it benchmarks the best

--- valen-format-png/src/main/java/be/twofold/valen/format/png/PngFilter.java
+++ valen-format-png/src/main/java/be/twofold/valen/format/png/PngFilter.java
@@ -37,88 +37,37 @@ final class PngFilter {
         //     return 0;
         // }
 
-        int[] sad = new int[5];
-        sad[0] = sad(curr, bpp, bpr);
-        filterSub(curr, prev, scratch[1], bpp, bpr);
-        sad[1] = sad(scratch[1], bpp, bpr);
-        filterUp(curr, prev, scratch[2], bpp, bpr);
-        sad[2] = sad(scratch[2], bpp, bpr);
-        filterAverage(curr, prev, scratch[3], bpp, bpr);
-        sad[3] = sad(scratch[3], bpp, bpr);
-        filterPaeth(curr, prev, scratch[4], bpp, bpr);
-        sad[4] = sad(scratch[4], bpp, bpr);
-
-        int bestRow = 0;
-        int bestSad = sad[0];
-        for (int i = 1; i < 5; i++) {
-            if (sad[i] < bestSad) {
-                bestRow = i;
-                bestSad = sad[i];
-            }
-        }
-
-        if (bestRow == 0) {
-            System.arraycopy(curr, bpp, scratch[0], bpp, bpr);
-        }
-
-        return bestRow;
-    }
-
-    private static void filterSub(byte[] curr, byte[] prev, byte[] out, int bpp, int bpr) {
-        for (int i = bpp; i < bpp + bpr; i++) {
-            int x = Byte.toUnsignedInt(curr[i]);
-            int a = Byte.toUnsignedInt(curr[i - bpp]);
-
-            out[i] = (byte) (x - a);
-        }
-    }
-
-    private static void filterUp(byte[] curr, byte[] prev, byte[] out, int bpp, int bpr) {
-        for (int i = bpp; i < bpp + bpr; i++) {
-            int x = Byte.toUnsignedInt(curr[i]);
-            int b = Byte.toUnsignedInt(prev[i]);
-
-            out[i] = (byte) (x - b);
-        }
-    }
-
-    private static void filterAverage(byte[] curr, byte[] prev, byte[] out, int bpp, int bpr) {
-        for (int i = bpp; i < bpp + bpr; i++) {
-            int x = Byte.toUnsignedInt(curr[i]);
-            int a = Byte.toUnsignedInt(curr[i - bpp]);
-            int b = Byte.toUnsignedInt(prev[i]);
-
-            out[i] = (byte) (x - (a + b >> 1));
-        }
-    }
-
-    private static void filterPaeth(byte[] curr, byte[] prev, byte[] out, int bpp, int bpr) {
+        byte[] sRow = scratch[1];
+        byte[] uRow = scratch[2];
+        byte[] aRow = scratch[3];
+        byte[] pRow = scratch[4];
+        int sadN = 0, sadS = 0, sadU = 0, sadA = 0, sadP = 0;
         for (int i = bpp; i < bpp + bpr; i++) {
             int x = Byte.toUnsignedInt(curr[i]);
             int a = Byte.toUnsignedInt(curr[i - bpp]);
             int b = Byte.toUnsignedInt(prev[i]);
             int c = Byte.toUnsignedInt(prev[i - bpp]);
 
-            out[i] = (byte) (x - paeth(a, b, c));
-        }
-    }
-
-    private static int sad(byte[] array, int offset, int length) {
-        int lim = offset + length;
-        int sad0 = 0, sad1 = 0, sad2 = 0, sad3 = 0;
-        int i;
-        for (i = offset; i < lim - 4; i += 4) {
-            sad0 += Math.abs(array[i/**/]);
-            sad1 += Math.abs(array[i + 1]);
-            sad2 += Math.abs(array[i + 2]);
-            sad3 += Math.abs(array[i + 3]);
+            sadN += Math.abs(curr[i]);
+            sadS += Math.abs(sRow[i] = (byte) (x - a));
+            sadU += Math.abs(uRow[i] = (byte) (x - b));
+            sadA += Math.abs(aRow[i] = (byte) (x - ((a + b) >>> 1)));
+            sadP += Math.abs(pRow[i] = (byte) (x - paeth(a, b, c)));
         }
 
-        int sad = sad0 + sad1 + sad2 + sad3;
-        for (; i < lim; i++) {
-            sad += Math.abs(array[i]);
+        int[] sad = {sadN, sadS, sadU, sadA, sadP};
+        int filter = 0;
+        int minSad = sad[0];
+        for (int i = 1; i < 5; i++) {
+            if (sad[i] < minSad) {
+                filter = i;
+                minSad = sad[i];
+            }
         }
-        return sad;
+        if (filter == 0) {
+            System.arraycopy(curr, bpp, scratch[0], bpp, bpr);
+        }
+        return filter;
     }
 
     private static int paeth(int a, int b, int c) {
