07709899baa3cd2e558463b3be6c1dbb3e766268
Jan De Kock <jan@twofold.be>
2025-12-06 20:15:33 +0100

    Inline class

--- valen-core/src/main/java/be/twofold/valen/core/game/Container.java
+++ valen-core/src/main/java/be/twofold/valen/core/game/Container.java
@@ -19,7 +19,36 @@ public interface Container<K, V> extends Closeable {
     Bytes read(K key, Integer size) throws IOException;
 
     static <K, V> Container<K, V> compose(List<Container<K, V>> containers) {
-        return new ContainerComposite<>(containers);
+        return new Container<K, V>() {
+            @Override
+            public Optional<V> get(K key) {
+                return containers.stream()
+                    .flatMap(file -> file.get(key).stream())
+                    .findFirst();
+            }
+
+            @Override
+            public Stream<V> getAll() {
+                return containers.stream().flatMap(Container::getAll);
+            }
+
+            @Override
+            public Bytes read(K key, Integer size) throws IOException {
+                for (var file : containers) {
+                    if (file.exists(key)) {
+                        return file.read(key, size);
+                    }
+                }
+                throw new FileNotFoundException("Unknown key: " + key);
+            }
+
+            @Override
+            public void close() throws IOException {
+                for (var file : containers) {
+                    file.close();
+                }
+            }
+        };
     }
 
 }
