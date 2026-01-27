c48e84c8312734e0b63bafb4288d412c8032648f
Jan De Kock <jan@twofold.be>
2026-01-27 23:00:24 +0100

    Update StreamDb template

--- valen-game-eternal/src/main/java/be/twofold/valen/game/eternal/reader/streamdb/StreamDbHeader.java
+++ valen-game-eternal/src/main/java/be/twofold/valen/game/eternal/reader/streamdb/StreamDbHeader.java
@@ -6,29 +6,21 @@ import java.io.*;
 import java.util.*;
 
 public record StreamDbHeader(
-    long magic,
     int headerLength,
-    int pad0,
-    int pad1,
-    int pad2,
     int numEntries,
     Set<StreamDbHeaderFlags> flags
 ) {
     public static StreamDbHeader read(BinarySource source) throws IOException {
-        var magic = source.readLong();
+        source.expectLong(0x61C7F32E29C2A550L); // magic
         var headerLength = source.readInt();
-        var pad0 = source.readInt();
-        var pad1 = source.readInt();
-        var pad2 = source.readInt();
+        source.expectInt(0x0); // pad0
+        source.expectInt(0x0); // pad1
+        source.expectInt(0x0); // pad2
         var numEntries = source.readInt();
         var flags = StreamDbHeaderFlags.read(source);
 
         return new StreamDbHeader(
-            magic,
             headerLength,
-            pad0,
-            pad1,
-            pad2,
             numEntries,
             flags
         );
