ec3f6b73ce2926a8d7fe9b95253704373c9a38b7
Jan De Kock <jan@twofold.be>
2026-07-11 21:49:05 +0200

    Model the side panel as an enum, drop suppressToggleEvents

---
+++ valen-ui/src/main/java/be/twofold/valen/ui/component/main/SidePanel.java
@@ -0,0 +1,11 @@
+package be.twofold.valen.ui.component.main;
+
+/**
+ * Which panel occupies the side of the main split pane, if any. Preview and
+ * settings are mutually exclusive.
+ */
+public enum SidePanel {
+    NONE,
+    PREVIEW,
+    SETTINGS
+}
