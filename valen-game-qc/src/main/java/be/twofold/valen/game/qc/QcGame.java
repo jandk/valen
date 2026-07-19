6b7decaae9da2b26721153a4766b257d266eb747
Jan De Kock <jan@twofold.be>
2026-07-19 20:29:50 +0200

    Add Quake Champions support with new game module and core functionality

---
+++ valen-game-qc/src/main/java/be/twofold/valen/game/qc/QcGame.java
@@ -0,0 +1,65 @@
+package be.twofold.valen.game.qc;
+
+import be.twofold.valen.core.game.*;
+import be.twofold.valen.core.util.*;
+import be.twofold.valen.game.qc.reader.pak.*;
+import wtf.reversed.toolbox.io.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class QcGame implements Game {
+    private final Path paksPath;
+    private final List<String> archiveNames;
+
+    public QcGame(Path clientPath) throws IOException {
+        this.paksPath = clientPath.resolve("preload", "paks");
+
+        try (var stream = Files.list(paksPath)) {
+            this.archiveNames = stream
+                .filter(p -> p.getFileName().toString().endsWith(".pak"))
+                .map(p -> Filenames.getBaseName(p.getFileName().toString()))
+                .sorted()
+                .toList();
+        }
+    }
+
+    @Override
+    public List<String> archiveNames() {
+        return archiveNames;
+    }
+
+    @Override
+    public AssetLoader open(String name) throws IOException {
+        Path resolved = paksPath.resolve(name + ".pak");
+        var source = BinarySource.open(resolved);
+
+        Pak pak = Pak.read(source);
+        var assets = pak.entries().stream()
+            .map(entry -> mapEntry(entry, resolved))
+            .toList();
+
+        var archive = Archive.of(assets);
+        var storageManager = new StorageManager(Map.of(resolved, source), Set.of(), new Decompressors(null));
+
+        return new AssetLoader(archive, storageManager, List.of());
+    }
+
+    private QcAsset mapEntry(PakEntry entry, Path path) {
+        Location location = new Location.FileSlice(path,
+            entry.relativeOffsetOfLocalHeader() + 30,
+            Math.toIntExact(entry.compressedSize()));
+
+        if (entry.compressionMethod() == 0) {
+            // Do nothing
+        } else if (entry.compressionMethod() == 8) {
+            int size = Math.toIntExact(entry.uncompressedSize());
+            location = new Location.Compressed(location, CompressionType.DEFLATE_RAW, size);
+        } else {
+            throw new UnsupportedOperationException("Unsupported compression method: " + entry.compressionMethod());
+        }
+
+        return new QcAsset(new QcAssetId(entry.fileName()/*.replace('\\', '/')*/), location);
+    }
+}
